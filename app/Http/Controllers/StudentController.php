b89c9688ced6596705e79a81b07757d411c76285
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-18 17:41:56 +0600

    Add User Bug Fixed

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,27 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Users;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
-    function getUser(){
+    function getUser()
+    {
         return Users::all();
     }
 
-    function createUser(Request $request){
+
+    function createUser(Request $request)
+    {
+        $rules = array(
+            'name' => 'required|string|max:50|min:3',
+            'email' => 'required|string|email|max:255',
+            'age' => 'required|integer',
+            'phone' => 'required|string|min:11|max:11'
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->failed()){
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
         $user = new Users();
-        $user->name = request('name');
-        $user->email = request('email');
-        $user->age = request('age');
-        $user->phone = request('phone');
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->age = $request->age;
+        $user->phone = $request->phone;
         $user->save();
 
         return response()->json(['message' => 'User created successfully'], 201);
-
     }
 
     //Update student
-    function updateUser(Request $request, $id){
+    function updateUser(Request $request, $id)
+    {
         $user = Users::find($id);
         $user->name = request('name');
         $user->email = request('email');
@@ -36,7 +53,8 @@ class StudentController extends Controller
     }
 
     //Delete student
-    function deleteUser($id){
+    function deleteUser($id)
+    {
         $user = Users::find($id);
         $user->delete();
 
@@ -44,7 +62,8 @@ class StudentController extends Controller
     }
 
     //Get student by name
-    function searchUserByName($name){
-        return Users::where('name', 'LIKE', '%'.$name.'%')->get();
+    function searchUserByName($name)
+    {
+        return Users::where('name', 'LIKE', '%' . $name . '%')->get();
     }
 }
